2418b9649bccd69366c1d41890c8f46ec3c63f54
Jesse Songok <jessesongok@gmail.com>
2019-11-21 12:22:36 +0300

    # Employees Delete  GIF by ID -> Created DB Delete Action (SQL)

--- actions/Gifs.js
+++ actions/Gifs.js
@@ -4,18 +4,25 @@ const db = require('../config/db');
 Insert New User into the user database table
  */
 const createGif = (id, gifname, imageurl, userid, created_at, modified_at) =>{
-    const query = `
+    const myquery = `
     INSERT INTO gifs(id, gifname, imageurl, userid, created_at, modified_at)VALUES($1,$2,$3,$4,$5,$6)RETURNING *`;
-    return db.query(query,[id, gifname, imageurl, userid, created_at, modified_at]);
+    return db.query(myquery,[id, gifname, imageurl, userid, created_at, modified_at]);
 };
 
 const getGifById = (id) =>{
     const query = `
     SELECT * FROM gifs WHERE id = $1`;
     return db.query(query,[id]);
-}
+};
+
+const deleteGifById = (id,userID)=>{
+    const dquery = `
+        DELETE FROM gifs WHERE id = $1 AND userid= $2`;
+    return db.query(dquery,[id,userID]);
+};
 
 module.exports ={
     createGif,
-    getGifById
+    getGifById,
+    deleteGifById
 };
